7e14444e950844de9976d146df75ddac70b8ee69
devjun20 <fwccjs@gmail.com>
2023-11-16 17:27:27 +0900

    로그인,  jwt 유저관리 구현

---
+++ src/main/java/com/ssafy/vue/member/model/mapper/MemberMapper.java
@@ -0,0 +1,18 @@
+package com.ssafy.vue.member.model.mapper;
+
+import com.ssafy.vue.member.model.MemberDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+@Mapper
+public interface MemberMapper {
+
+	MemberDto login(MemberDto memberDto) throws SQLException;
+	MemberDto userInfo(String userId) throws SQLException;
+	void saveRefreshToken(Map<String, String> map) throws SQLException;
+	Object getRefreshToken(String userid) throws SQLException;
+	void deleteRefreshToken(Map<String, String> map) throws SQLException;
+	
+}
